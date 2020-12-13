6afef0093af597be161228e4c6c029ae240d80af
Benjamin Wong <44148480+chiyeon@users.noreply.github.com>
2020-12-12 17:56:14 -0800

    Added snowfall.cpp

---
+++ snowfall.cpp
@@ -0,0 +1,92 @@
+#include <stdlib.h>	// srand n rand
+#include <cstdio>		// I/O
+#include <time.h>		// time()
+
+#ifdef _WIN32
+#include <windows.h>
+#else
+#include <unistd.h>
+#endif					// for clearing the screen
+
+using namespace std;
+
+char world[14][40] = {
+	"_______________________________________",
+	"|                                     |",
+	"|                                     |",
+	"|                                     |",
+	"|                                     |",
+	"|                                     |",
+	"|                                     |",
+	"|                            __       |",
+	"|                           /  \\      |",
+	"|                          /    \\     |",
+	"|                         /      \\    |",
+	"|       ______           /        \\___|",
+	"|______/      \\_________/             |",
+	"|_____________________________________|"
+};
+
+int main() {
+	
+	printf("enjoy the shnow :)\n\n");
+	
+	srand (time(0));
+	
+	for(;;) {
+		#ifdef _WIN32
+		system("cls");
+		#else
+		system("clear");
+		#endif
+		printf("             > snowfall <\n");
+		
+		// iterate through each character in the map
+		for(int y = 14; y > 0; y--) {
+			for(int x = 39; x > 0; x--) {
+				// render each character
+				int printX = 39 - x;
+				int printY = 14 - y;
+				printf("%c", world[printY][printX]);
+				
+				// new line when necessary
+				if(printX == 38)
+					printf("\n");
+				
+				// randomly spawn snowflakes on the top row
+				if(y == 1) {
+					if(world[y][x] != '|') {
+						if(rand() % 35 == 0) {
+							world[y][x] = '*';
+							continue;
+						}
+					}
+				}
+				
+				// character logic
+				switch(world[y][x]) {
+					case 'v': world[y][x] = '_'; break;		// _ hit particle. reverts back to original state
+					case '>': world[y][x] = '/'; break;		// / hit particle. reverts back to original state
+					case '<': world[y][x] = '\\'; break;	// \ hit particle. reverts back to original state
+					case '*':		// snowflake. Falls down gradually, causes impact particles.
+						world[y][x] = ' ';
+						if(world[y+1][x] == ' ') {
+							world[y+1][x] = '*';
+						} else {
+							world[y][x] = ' ';
+							if(world[y+1][x] == '_') {
+								world[y+1][x] = 'v';
+							} else if(world[y+1][x] == '/') {
+								world[y+1][x] = '>';
+							} else if(world[y+1][x] == '\\') {
+								world[y+1][x] = '<';
+							}
+						}
+						break;
+				}
+			}
+		}
+	}
+	return 0;
+}
+// don't forget to make kanye with brandon in ds3
