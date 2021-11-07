97012339eea72ec2b4f0cf693c96fd7caab6cf97
Benjamin Wong <chiyeonwastaken@gmail.com>
2021-11-06 17:47:33 -0700

    removed windows ver; fixed snowfalling (requires REVERSED loop)

--- snowfall.cpp
+++ snowfall.cpp
@@ -1,55 +1,31 @@
-#include <cstdlib>					// srand, rand, cls
-#include <cstdio>					// I/O
-#include <time.h>					// time
-#include <chrono>					// sleeping
-#include <thread>
-//config
-int sleepDuration = 100, snowflakeSpawnChance = 3, worldWidth = 80, worldHeight = 30;		// sleepDuration: ms between frames; spawn chance: % chance per tile to spawn *
+#include <cstdlib>					// srand, rand, clear
+#include <cstdio>					// i/o
+#include <time.h>					// time (seeding)
+#include <chrono>					// millisecond conversion
+#include <thread>					// sleeping thread
+//config : length of each 'frame'; chance for each top tile to spawn a snowflake; world dimensions
+int sleepDuration = 300, snowflakeSpawnChance = 3, worldWidth = 80, worldHeight = 30;
 int main() {
-	srand (time(0));
-	printf("enjoy the shnow :)\n\n");
-	char world[worldHeight][worldWidth];
-	for(int y = 0; y < worldHeight; y++) {
-		for(int x = 0; x < worldWidth; x++) {
-			world[y][x] = x == worldWidth - 1 ? '\n' : ' ';
-		}
-	}
+	srand(time(0));
+	char world[worldHeight * worldWidth];
+	for(int i = 0; i < worldHeight * worldWidth; i++)										// populate world w empty space & newlines
+		world[i] = i % worldWidth ? ' ' : '\n';
 	for(;;) {
-		std::this_thread::sleep_for(std::chrono::milliseconds(sleepDuration));
-		system("clear");
-		for(int y = worldHeight; y > 0; y--) {										// iterate thru map
-			for(int x = 0; x < worldWidth; x++) {								
-				switch(world[y][x]) {
-					case '.':
-					case '*':
-					case 'o':
-						if(world[y+1][x] == ' ') {
-							world[y+1][x] = world[y][x];
-							world[y][x] = ' ';
-						}
-						break;
-						/*
-						world[y][x] = ' ';
-						if(world[y+1][x] == ' ') world[y+1][x] = '*';		// move down when space
-						if(world[y+1][x] == '#') world[y][x] = '.';			// particles when not
-						if(world[y+1][x] == '.') world[y+1][x] = '#'; */
-				}
-				printf("%c", world[worldHeight - y][x]);								// print character
-				// if top row && within x bounds && chance && no snowflake below...spawn a snowflake!
-				//if(y == 1 && x > 3 && x < 40 && rand() % 100 < snowflakeSpawnChance && world[y+1][x] != '*') world[y][x] = '*';
-				//if(y == 1 && x > 3 && x < 40 && world[y+1][x] == ' ' && rand() % 100 < snowflakeSpawnChance) world[y][x] = rand() % 2 == 0 ? '*' : 'o';
-				if(y == 1 && x > 0 && x < worldWidth && world[y+1][x] == ' ' && rand() % 100 < snowflakeSpawnChance) {
-					int c = rand() % 2;
-					if(c == 0) {
-						world[y][x] = '*';
-					} else if(c == 1) {
-						world[y][x] = '.';
-					} else {
-						world[y][x] = 'o';
+		system("clear");																	// clear new frame
+		for(int i = worldHeight * worldWidth; i > 0; i--) {									// iterate (backwards to make snow fall evenly)
+			printf("%c", world[worldHeight * worldWidth - i]);								// render map (backwards to reverse loop)
+			switch(world[i]) {
+				case '*':
+				case '.':																	// make snowflakes fall
+					if(world[i + worldWidth] == ' ') {
+						world[i + worldWidth] = world[i];
+						world[i] = ' ';
 					}
-				}
-			}
+					break;
+			}																				// spawn new snowflakes under right conditions
+			if(i < worldWidth * 2 && world[i - worldWidth] == ' ' && rand() % 100 < snowflakeSpawnChance) world[i] = rand() % 2 == 0 ? '*' : '.';
 		}
+		std::this_thread::sleep_for(std::chrono::milliseconds(sleepDuration));				// sleep
 	}
 	return 0;
 } // don't forget to make kanye with brandon in ds3
